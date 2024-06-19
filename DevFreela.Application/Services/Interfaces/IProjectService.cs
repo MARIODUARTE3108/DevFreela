e03db4baa68a2a6059306b86ae8aad8e2aa40f7d
MARIO\mario <marioduarte.ivisa@gmail.com>
2024-06-19 15:08:56 -0300

    first commit

---
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Application.Models.InputModels;
+using DevFreela.Application.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll(string query);
+        ProjectDetailsViewModel GetById(int id);
+        int Create(NewProjectInputModel inputModel);
+        void Update(UpdateProjectInputModel inputModel);
+        void Delete(int id);
+        void CreateComment(CreateCommentInputModel inputModel);
+        void Start(int id);
+        void Finish(int id);
+    }
+}
---
+++ DevFreela.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using DevFreela.Application.Models.InputModels;
+using DevFreela.Application.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        UserViewModel GetUser(int id);
+        int Create(CreateUserInputModel inputModel);
+    }
+}
